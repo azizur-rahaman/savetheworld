272bec319a2518cbb9ac5eca7af5cdd8f8aa7958
Azizur Rahaman <frazizur.rahaman@gmail.com>
2023-07-08 02:14:02 +0600

    new chages

--- app/src/main/java/com/hassanpial/savingtheworld/MainActivity.java
+++ app/src/main/java/com/hassanpial/savingtheworld/MainActivity.java
@@ -1,46 +1,166 @@
 package com.hassanpial.savingtheworld;
-
 import static android.view.animation.Animation.INFINITE;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
-import android.graphics.PorterDuffXfermode;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieProperty;
 import com.airbnb.lottie.model.KeyPath;
 import com.airbnb.lottie.value.LottieValueCallback;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.hassanpial.savingtheworld.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.widget.Button;
-import android.widget.Toast;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText tvConvertedText;
+    private SpeechRecognizer speechRecognizer;
+    private Intent speechRecognizerIntent;
+    private boolean isListening = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeLottieFiles();
+        tvConvertedText = findViewById(R.id.editText);
+        Button btnSpeechToText = findViewById(R.id.btn);
 
 
+
+        // Request RECORD_AUDIO permission if not granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {}
+
+            @Override
+            public void onBeginningOfSpeech() {}
+
+            @Override
+            public void onRmsChanged(float rmsdB) {}
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {}
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {}
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (matches != null) {
+                    String convertedText = matches.get(0);
+                    tvConvertedText.setText(convertedText);
+
+                    if(convertedText.equals("drop water")){
+                       dropWaterWithTimer();
+
+                    }else{
+                        speechRecognizer.startListening(speechRecognizerIntent);
+                    }
+                }else{
+
+                }
+
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {}
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+
+
+        btnSpeechToText.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                View makeitgone = findViewById(R.id.makeItGone);
+                makeitgone.setVisibility(View.GONE);
+
+                btnSpeechToText.setVisibility(View.GONE);
+                if (!isListening) {
+                    startListening();
+                } else {
+                    stopListening();
+                }
+            }
+
+            private void startListening() {
+                speechRecognizer.startListening(speechRecognizerIntent);
+                isListening = true;
+
+            }
+
+            private void stopListening() {
+                speechRecognizer.stopListening();
+                isListening = false;
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (speechRecognizer != null) {
+            speechRecognizer.destroy();
+        }
+    }
+
+
+
+private  void initializeLottieFiles(){
         LottieAnimationView helicopterAnimationView = findViewById(R.id.helicopter);
         helicopterAnimationView.playAnimation();
         helicopterAnimationView.setRepeatCount(INFINITE);
@@ -65,30 +185,41 @@ public class MainActivity extends AppCompatActivity {
         fire5AnimationView.playAnimation();
         fire5AnimationView.setRepeatCount(INFINITE);
 
-
         LottieAnimationView waterDrop = findViewById(R.id.water);
-        waterDrop.setVisibility(View.GONE);
+        FrameLayout waterArea = findViewById(R.id.waterArea);
 
+        waterArea.setVisibility(View.GONE);
         int color = Color.parseColor("#d4f1f9");
         LottieValueCallback<ColorFilter> colorFilterCallback = new LottieValueCallback<>();
         colorFilterCallback.setValue(new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP));
         waterDrop.addValueCallback(new KeyPath("**"), LottieProperty.COLOR_FILTER, colorFilterCallback);
 
 
-
-        Button btn = findViewById(R.id.btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               waterDrop.setVisibility(View.VISIBLE);
-               waterDrop.playAnimation();
-               waterDrop.setRepeatCount(1);
-
-                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
     }
+    private void dropWaterWithTimer(){
+
+    FrameLayout waterArea = findViewById(R.id.waterArea);
+    waterArea.setVisibility(View.VISIBLE);
+
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) waterArea.getLayoutParams();
+
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+//
+//               if(params.topMargin < (480)){
+//                   params.setMargins(params.leftMargin, 200, params.rightMargin, params.bottomMargin);
+//                   waterArea.setLayoutParams(params);
+//               }
+
+
+            }
+        };
+
+        Timer time = new Timer();
+
+        time.scheduleAtFixedRate(timerTask, 5, 5);
+    }
+
 }
